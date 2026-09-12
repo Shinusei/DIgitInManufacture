a5608d202ad7b95b890f544bb6bc7c4b34ff0b26
Shinusei <lyshagrishin@gmail.com>
2026-09-12 12:48:58 +0300

    prac 1

---
+++ Prac1-2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите номер варианта (1-25):");
+int variant = Convert.ToInt32(Console.ReadLine());
+
+switch (variant)
+{
+    case 1: Variant1.Run(); break;
+    case 2: Variant2.Run(); break;
+    case 3: Variant3.Run(); break;
+    case 4: Variant4.Run(); break;
+    case 5: Variant5.Run(); break;
+    case 6: Variant6.Run(); break;
+    case 7: Variant7.Run(); break;
+    case 8: Variant8.Run(); break;
+    case 9: Variant9.Run(); break;
+    case 10: Variant10.Run(); break;
+    case 11: Variant11.Run(); break;
+    case 12: Variant12.Run(); break;
+    case 13: Variant13.Run(); break;
+    case 14: Variant14.Run(); break;
+    case 15: Variant15.Run(); break;
+    case 16: Variant16.Run(); break;
+    case 17: Variant17.Run(); break;
+    case 18: Variant18.Run(); break;
+    case 19: Variant19.Run(); break;
+    case 20: Variant20.Run(); break;
+    case 21: Variant21.Run(); break;
+    case 22: Variant22.Run(); break;
+    case 23: Variant23.Run(); break;
+    case 24: Variant24.Run(); break;
+    case 25: Variant25.Run(); break;
+    default: Console.WriteLine("Варианта с таким номером нет."); break;
+}
--- Prac1-2/obj/Debug/net10.0/Prac1-2.AssemblyInfo.cs
+++ Prac1-2/obj/Debug/net10.0/Prac1-2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prac1-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3d1242d4206dc5516ad551f1e0a7675ca67dba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prac1-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prac1-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
